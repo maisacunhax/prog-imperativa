6ca2dd55245875b4949ef622271fc1b69ef244cc
Maísa <93223764+maisacunhax@users.noreply.github.com>
2021-11-28 08:03:45 -0300

    Add files via upload

---
+++ aula08-extra.js
@@ -0,0 +1,18 @@
+//MINI DESAFIOS
+//01
+const status = true;
+
+if(status){
+    console.log('O valor é verdadeiro')
+}else{
+    console.log('O valor é falso')
+}
+
+//02 
+
+const linguagem = 'javascript'
+if(linguagem === 'javascript'){
+    console.log('Estou aprendendo')
+}else{
+    console.log('Aprenderei mais tarde')
+}
